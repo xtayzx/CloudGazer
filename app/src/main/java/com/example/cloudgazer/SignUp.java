45461489fab651c511f3e8305c9b99dbae42dbb8
am-na <68403945+am-na@users.noreply.github.com>
2021-11-06 22:48:57 -0700

    SignUp Layout
    Creating layout for Sign Up and Button Click Intent back to Log In

--- app/src/main/java/com/example/cloudgazer/SignUp.java
+++ app/src/main/java/com/example/cloudgazer/SignUp.java
@@ -2,7 +2,9 @@ package com.example.cloudgazer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUp extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
+
+    public void signedUp(View v){
+        Intent i  = new Intent(this, Welcome.class);
+        startActivity(i);
+    }
+
 }
