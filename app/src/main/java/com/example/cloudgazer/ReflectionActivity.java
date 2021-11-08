1bab0ee4bb25728ede8855eace2e6e3290ec4ae0
Taylen LC <taylen@shaw.ca>
2021-11-07 16:10:29 -0800

    moved stuff around

--- app/src/main/java/com/example/cloudgazer/ReflectionActivity.java
+++ app/src/main/java/com/example/cloudgazer/ReflectionActivity.java
@@ -1,4 +1,78 @@
 package com.example.cloudgazer;
 
-public class ReflectionActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+public class ReflectionActivity extends Activity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+    private SeekBar thisSeekBar;
+    private int rateDayValue;
+    EditText rateDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reflection_layout);
+
+        rateDay = (EditText)findViewById(R.id.rateDay);
+
+        thisSeekBar = (SeekBar)findViewById(R.id.seekBar);
+        thisSeekBar.setOnSeekBarChangeListener(examSeekBarListener);
+    }
+
+    private SeekBar.OnSeekBarChangeListener examSeekBarListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            rateDayValue = thisSeekBar.getProgress();
+            //rateDay.setText(rateDayValue);
+            rateDay.setText(String.valueOf(rateDayValue));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(seekBar.getId() == thisSeekBar.getId()) {
+            rateDayValue = seekBar.getProgress();
+            rateDay.setText(rateDayValue);
+        }
+    }
+
+//    @Override
+//    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//    }
+//
+//    @Override
+//    public void afterTextChanged(Editable editable) {
+//
+//    }
+
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
