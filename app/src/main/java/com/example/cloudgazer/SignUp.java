8b53af8a7ffbeef6b802be51113facc9f20fc8a8
Taylen LC <taylen@shaw.ca>
2021-11-08 18:16:36 -0800

    implementation of sharedprefs for saved data
    still needs to be tested for bugs

--- app/src/main/java/com/example/cloudgazer/SignUp.java
+++ app/src/main/java/com/example/cloudgazer/SignUp.java
@@ -2,19 +2,31 @@ package com.example.cloudgazer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    EditText nameEditText, emailEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        nameEditText = findViewById(R.id.nameEntry);
+        emailEditText = findViewById(R.id.emailEntry);
+        passwordEditText = findViewById(R.id.passwordEntry);
     }
 
     public void signedUp(View v){
+        submit(v);
         Intent i  = new Intent(this, Welcome.class);
         startActivity(i);
     }
@@ -22,6 +34,16 @@ public class SignUp extends AppCompatActivity {
     public void reflectionActivity(View v){
         Intent i  = new Intent(this, ReflectionActivity.class);
         startActivity(i);
+    }
+
+    public void submit(View view){
+        SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString("username", nameEditText.getText().toString());
+        editor.putString("email", emailEditText.getText().toString());
+        editor.putString("password", passwordEditText.getText().toString());
+        Toast.makeText(this, "Username, email and password have been saved to Preferences", Toast.LENGTH_LONG).show();
+        editor.commit();
     }
 
 }
--- app/src/main/java/com/example/cloudgazer/Welcome.java
+++ app/src/main/java/com/example/cloudgazer/Welcome.java
@@ -1,18 +1,26 @@
 package com.example.cloudgazer;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Welcome extends AppCompatActivity {
+
+    public static final String DEFAULT = "not available";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        //check to see if there is anything saved under sharedPreferences
+        checkData();
     }
 
     public void logIn(View v){
@@ -24,6 +32,23 @@ public class Welcome extends AppCompatActivity {
     public void accountCreate (View v){
         Intent i = new Intent(this, SignUp.class);
         startActivity(i);
+    }
+
+    public void checkData() {
+        SharedPreferences sharedPrefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+
+        String savedUsername = sharedPrefs.getString("username", DEFAULT);
+        String savedEmail = sharedPrefs.getString("email", DEFAULT);
+        String savedPassword = sharedPrefs.getString("password", DEFAULT);
+
+        Log.i("TESTING", "Saved Username: "+savedUsername+" || Saved Password: "+savedPassword+" || Saved Email: "+savedEmail);
+
+        //add email later
+        if((savedUsername.isEmpty() && savedPassword.isEmpty()) && savedEmail.isEmpty() || savedUsername.equals(DEFAULT) && savedPassword.equals(DEFAULT) && savedEmail.equals(DEFAULT)) {
+            Toast.makeText(this, "No saved username and password. Create a new username and password.", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, SignUp.class);
+            startActivity(intent);
+        }
     }
 
 }
