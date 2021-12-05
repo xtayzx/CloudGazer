dc44bacd7ae2b1bb2ea14aa254b21a7a6dc0ce0e
Taylen LC <taylen@shaw.ca>
2021-12-05 15:59:05 -0800

    blue buttons and appearance

--- app/src/main/java/com/example/cloudgazer/ViewEntryActivity.java
+++ app/src/main/java/com/example/cloudgazer/ViewEntryActivity.java
@@ -137,8 +137,10 @@ public class ViewEntryActivity extends Activity {
                                 "This entry has been deleted",Toast.LENGTH_SHORT).show();
                         Log.i("VALUE", String.valueOf(titleField.getText()));
                         db.deleteRow(String.valueOf(titleField.getText()));
-                        deleteButton.setText("Entry Deleted");
-                        deleteButton.setBackgroundColor(Color.BLACK);
+                        deleteButton.setVisibility(View.INVISIBLE);
+//                        deleteButton.setText("Entry Deleted");
+//                        deleteButton.setBackgroundColor(Color.BLACK);
+
                     }
                 });
 
