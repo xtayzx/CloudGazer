a4c705a688d48329dfe3ae4e156073092c8616ec
Taylen LC <taylen@shaw.ca>
2021-12-06 17:56:53 -0800

    removal of tapping the recycler view to see which is selected

--- app/src/main/java/com/example/cloudgazer/MyAdapter.java
+++ app/src/main/java/com/example/cloudgazer/MyAdapter.java
@@ -72,9 +72,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(context,
-                    "You have clicked " + ((TextView)view.findViewById(R.id.titleRow)).getText().toString(),
-                    Toast.LENGTH_SHORT).show();
+//            Toast.makeText(context,
+//                    "You have clicked " + ((TextView)view.findViewById(R.id.titleRow)).getText().toString(),
+//                    Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(view.getContext(), ViewEntryActivity.class);
             intent.putExtra("title",((TextView)view.findViewById(R.id.titleRow)).getText().toString() );
